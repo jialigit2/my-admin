38e4b06f6f2c196e5529e330968795abd7578a10
jialigit <1392029926@qq.com>
2025-08-13 17:10:34 +0800

    feature components

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+0. main: project vue + vite 
+1. branch br_first_component
+第一个计数器组件
+
+feature 1 : button + -
+feature 2: 负数是红色
+
+
---
+++ src/components/Counter.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const props = defineProps({
+    initial: {
+        type: Number,
+        default:0
+    }
+})
+// 定义一个响应式变量 count
+const count = ref(props.initial)
+
+// 定义一个方法来加一
+function increment() {
+  count.value++
+}
+
+function decresement() {
+  count.value--;
+}
+
+const countColor = computed(()=> {
+  if (count.value > 0) {
+    return 'green'
+  }else if(count.value ==0){
+    return 'black'
+  }else{
+    return 'red'
+  }
+})
+</script>
+
+<template>
+  <div style="text-align: center; padding-top: 50px;">
+    <h1>Vue3 计数器</h1>
+    <p :style="{color: countColor}">当前计数：{{ count }}</p>
+    <button @click="increment">+1</button>
+    <button @click="decresement">-1</button>
+  </div>
+</template>
+
+<style>
+button {
+  padding: 8px 16px;
+  font-size: 16px;
+}
+</style>
